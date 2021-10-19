c80f2f019557c842137a60d90429f0b9f6c7e02c
AinunAns <90886374+AinunAns@users.noreply.github.com>
2021-10-19 21:08:55 +0700

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,10 @@
+<?php
+echo "<h2> Selamat berlatih menjadi kolaborator </h2>";
+echo "<h3>Isi Nama dan Nim Anda di bawah baris code ini</h3>";
+<<<<<<< HEAD
+echo "<h3>Ng. Ainun Annisa N.I.M.(155610024)</h3>"
+=======
+echo "<h3>Ng. Satria Utomo W.P.(155610024)</h3>"
+>>>>>>> upstream/main
+
+?>
